ad849451afc9a2aad5d707d3ed994396879de264
csanti <cavannaprogramacion@gmail.com>
2023-09-28 13:35:07 -0300

    Funcionalidad principal realizada, todo por prompts, gestor de gastos. PreEntrega2 Cavanna Santiago

--- js/main.js
+++ js/main.js
@@ -1,27 +1,92 @@
+let totalDinero = parseFloat(prompt("Ingresa el total de dinero que tienes:"));
 
-// // //Convertir valor a dolares
-// function convertirEnDolares(valorEnPesos){
-//     const tasaCambioDolares = 0.0028; //Tasa de cambio al dia 8/9/2023 (1 ARS = 0.0028)
-//     return valorEnPesos * tasaCambioDolares;
-// };
+// Almacena gastos por categorías
+const gastosPorCategoria = {};
 
-// // Conversor
+function registrarGasto() {
+    const nombreGasto = prompt("Ingresa el nombre del gasto (o escribe 'fin' para salir):");
 
-//     function convertirMoneda(){
-//         while(true){
-//         let valorEnPesos = parseFloat(prompt("Ingrese el valor en pesos argentinos: "));
-//         if (isNaN(valorEnPesos)){
-//             alert("Por favor, ingresar un numero valido.");
-//             return;
-//         }
-//         let valorEnDolares= convertirEnDolares(valorEnPesos);
+    if (nombreGasto.toUpperCase() === 'FIN') {
+        return;
+    }
 
-//         console.log(`${valorEnPesos} ARS es igual a ${valorEnDolares.toFixed(2)} USD.`);
-//     };
-// }
+    const categoriaGasto = prompt("Ingresa la categoría del producto (o escribe 'fin' para salir)");
+if (categoriaGasto.toUpperCase() === 'FIN') {
+    return;
+}
 
+const categoriaMayusculas = categoriaGasto.toUpperCase();
 
+const precioGasto = parseFloat(prompt("Ingresa el precio del gasto:"));
+totalDinero -= precioGasto;
 
-// //Llama al conversor
-// convertirMoneda();
+  // Registrar el gasto en la categoría correspondiente
+if (!gastosPorCategoria[categoriaMayusculas]) {
+    gastosPorCategoria[categoriaMayusculas] = [];
+}
+gastosPorCategoria[categoriaMayusculas].push({ nombre: nombreGasto, precio: precioGasto });
 
+console.log(`Gasto registrado: ${nombreGasto}, Categoría: ${categoriaMayusculas}, Precio: $${precioGasto}`);
+console.log(`Saldo restante: $${totalDinero}`);
+
+  registrarGasto();
+}
+
+registrarGasto();
+
+console.log("Fin del registro de gastos. Tu saldo final es: $" + totalDinero);
+console.log("Gastos por categoría:");
+console.log(gastosPorCategoria);
+// -------------------------------------------------------------
+
+// Buscar gastos por categoría o filtrar palabra
+while (true) {
+    const opcionBusqueda = prompt("¿Deseas buscar por categoría o por producto? (Escribe 'categoria' o 'producto' o 'fin' para salir)");
+    if (opcionBusqueda.toUpperCase() === 'FIN') {
+    console.log("Búsqueda finalizada.");
+    break;
+    }
+
+    if (opcionBusqueda.toLowerCase() === 'categoria') {
+    const categoriaBuscada = prompt("Ingrese la categoría a buscar:");
+    const categoriaBuscadaMayusculas = categoriaBuscada.toUpperCase();
+    const gastosEnCategoria = gastosPorCategoria[categoriaBuscadaMayusculas] || [];
+
+    if (gastosEnCategoria.length === 0) {
+        console.log(`No se encontraron gastos en la categoría "${categoriaBuscadaMayusculas}".`);
+    } else {
+        console.log(`Gastos en la categoría "${categoriaBuscadaMayusculas}":`);
+        gastosEnCategoria.forEach((gasto, index) => {
+        console.log(`Producto ${index + 1}:`);
+        console.log(`Nombre: ${gasto.nombre}`);
+        console.log(`Precio: $${gasto.precio}`);
+        });
+    };
+    } else if (opcionBusqueda.toLowerCase() === 'producto') {
+    const productoBuscado = prompt("Ingrese el nombre del producto a buscar:");
+    const gastosFiltrados = [];
+
+    for (const categoria in gastosPorCategoria) {
+        gastosPorCategoria[categoria].forEach(gasto => {
+        if (gasto.nombre.toLowerCase().includes(productoBuscado.toLowerCase())) {
+            gastosFiltrados.push(gasto);
+        }
+        });
+    }
+
+    if (gastosFiltrados.length === 0) {
+        console.log(`No se encontraron gastos que contengan "${productoBuscado}".`);
+    } else {
+        console.log("Gastos que contienen la palabra '" + productoBuscado + "':");
+
+        gastosFiltrados.forEach((gasto, index) => {
+        console.log(`Producto ${index + 1}:`);
+        console.log(`Nombre: ${gasto.nombre}`);
+        console.log(`Precio: $${gasto.precio}`);
+        });
+        console.log(gastosFiltrados);
+    }
+    } else {
+    console.log("Opción no válida. Por favor, elige 'categoria', 'producto' o 'fin'.");
+    }
+};
