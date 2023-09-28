9e8bf65dd05046c716f6596a158abb7744f93c6a
csanti <cavannaprogramacion@gmail.com>
2023-09-28 00:32:25 -0300

    Hecho toda la estructura, falta javascript

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 
-// //Convertir valor a dolares
-function convertirEnDolares(valorEnPesos){
-    const tasaCambioDolares = 0.0028; //Tasa de cambio al dia 8/9/2023 (1 ARS = 0.0028)
-    return valorEnPesos * tasaCambioDolares;
-};
+// // //Convertir valor a dolares
+// function convertirEnDolares(valorEnPesos){
+//     const tasaCambioDolares = 0.0028; //Tasa de cambio al dia 8/9/2023 (1 ARS = 0.0028)
+//     return valorEnPesos * tasaCambioDolares;
+// };
 
-// Conversor
+// // Conversor
 
-    function convertirMoneda(){
-        while(true){
-        let valorEnPesos = parseFloat(prompt("Ingrese el valor en pesos argentinos: "));
-        if (isNaN(valorEnPesos)){
-            alert("Por favor, ingresar un numero valido.");
-            return;
-        }
-        let valorEnDolares= convertirEnDolares(valorEnPesos);
+//     function convertirMoneda(){
+//         while(true){
+//         let valorEnPesos = parseFloat(prompt("Ingrese el valor en pesos argentinos: "));
+//         if (isNaN(valorEnPesos)){
+//             alert("Por favor, ingresar un numero valido.");
+//             return;
+//         }
+//         let valorEnDolares= convertirEnDolares(valorEnPesos);
 
-        alert(`${valorEnPesos} ARS es igual a ${valorEnDolares.toFixed(2)} USD.`);
-    };
-}
+//         console.log(`${valorEnPesos} ARS es igual a ${valorEnDolares.toFixed(2)} USD.`);
+//     };
+// }
 
 
 
-//Llama al conversor
-convertirMoneda();
+// //Llama al conversor
+// convertirMoneda();
 
